b255dfdd33089d412da853f18f95b8cd1f179fed
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-06-09 09:56:11 -0300

    Cria as media-queries necessárias para uma correta visualização em celulares, tablets e desktops

---
+++ js/responsive.js
@@ -0,0 +1,59 @@
+function start() {
+    const navigation = document.getElementById('navigation')
+    const width = screen.width
+
+    // navigation = width <= 600 ? console.log('Mobile') : console.log('Web');
+
+    if (width <= 625) {
+        createMobileNavigationBar(navigation)
+    } else {
+        createWebNavigationBar(navigation)
+    }
+}
+
+const createMobileNavigationBar = (navigation) => {
+    navigation.classList.add('menuMobile')
+    navigation.innerHTML =
+    `
+        <ul>
+            <li>
+                <div class="dropdown">
+                   <a href="#"><i class="fas fa-bars"></i></a>
+                    <div class="dropdown-content">
+                     <a href="#">Jogos</a>
+                     <a href="#">Notícias</a>
+                     <a href="#">Sobre</a>
+                     <a href="#">Contato</a>
+                     <a href="#">Login</a>
+                     <a href="#">Cadastre-se</a>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    `
+}
+
+const createWebNavigationBar = (navigation) => {
+    navigation.classList.add('menuWeb')
+    navigation.innerHTML =
+    `
+        <ul>
+           <li><a href="#">Jogos</a></li>
+           <li><a href="#">Notícias</a></li>
+           <li><a href="#">Sobre</a></li>
+           <li><a href="#">Contato</a></li>
+           <li>
+                <div class="dropdown">
+                   <a href="#">Entrar
+                      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/236520/arrow_down.svg"
+                       alt="Submenu open">
+                    </a>
+                   <div class="dropdown-content">
+                       <a href="#">Login</a>
+                       <a href="#">Cadastre-se</a>
+                   </div>
+                </div>
+            </li>
+        </ul>
+    `
+}
